6f08b47e891b33ce85e512314148de966f6fe5f2
Kazuma Saito <kazu15870@gmail.com>
2023-09-19 16:24:39 -0700

    complete ABC275-A

---
+++ ABC275/A/Main.java
@@ -0,0 +1,28 @@
+/**********
+ * 
+ * 線形探索
+ * 
+**********/
+
+// package ABC275.A;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int N = sc.nextInt();
+
+      int highest = 0;
+      int count = 0;
+      for (int i = 0; i < N; i++) {
+        int height = sc.nextInt();
+
+        if(highest < height) {
+          highest = height;
+          count = i + 1;
+        }
+      }
+      System.out.println(count);
+  }
+}
