1cb33f1497837fcafa9183e359337c47afd5a2e8
Kazuma Saito <kazu15870@gmail.com>
2023-09-25 08:20:14 -0700

    ABC213-B

---
+++ ABC213/B/Main.java
@@ -0,0 +1,37 @@
+/*********
+  全探索
+**********/
+// package ABC213.B;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int N = sc.nextInt();
+      int[] num  = new int[N];
+
+      for (int i = 0; i < num.length; i++) {
+        num[i] = sc.nextInt();
+      }
+
+      int tallest = 0;
+      int secondTallest = 0;
+
+      for (int i = 0; i < num.length; i++) {
+        if(num[i] > tallest) {
+          secondTallest = tallest;
+          tallest = num[i];
+        } else if(num[i] != tallest && num[i] > secondTallest) {
+          secondTallest = num[i];
+        }
+      }
+
+      for (int i = 0; i < num.length; i++) {
+        if(num[i] == secondTallest) {
+          System.out.println(i + 1);
+          break;
+        }
+      }
+  }
+}
