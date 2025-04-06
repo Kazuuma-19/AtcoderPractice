01e8e32e4ab435a6631ae0fedc29eaaa5ab8f188
Kazuma <kazu15870@gmail.com>
2025-04-06 13:07:47 +0900

    AC: ABC389.C

--- ABC389/C/Main.java
+++ ABC389/C/Main.java
@@ -1,54 +1,66 @@
-package ABC389.C;
+        package ABC389.C;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.Scanner;
 
-/**
- * TODO:
- * - ロジックが違う。データ構造から考え直したほうが良さそう
- * - リファクタリング
- */
-public class Main {
-    public static void getInputs() {
-        List<Integer> lengthOfSnakes = new ArrayList<>();
-        List<Integer>indexOfSnakes = new ArrayList<>();
-
-        Scanner sc = new Scanner(System.in);
-        int numberOfQuery = sc.nextInt();
-
-        for(int i = 0; i < numberOfQuery; i++) {
-            int queryType = sc.nextInt();
-            if (queryType == 1) {
-                // type 1(add snake)
-                int snakeLength = sc.nextInt();
-                System.out.println("output: " + queryType + " " + snakeLength);
-
+        /**
+         * TLE
+         * note:
+         * - intは32bit。10^9くらいしか対応できない
+         * - List.removeはO(N)かかる
+         */
+        public class Main {
+            /**
+             * type 1(add snake)
+             * @param snakeLength
+             * @param lengthOfSnakes
+             * @param headIndexOfSnakes
+             */
+            public static void addSnake(long snakeLength, List<Long> lengthOfSnakes, List<Long> headIndexOfSnakes) {
                 if(lengthOfSnakes.isEmpty()) {
-                    indexOfSnakes.add(0);
+                    headIndexOfSnakes.add(0L);
                 } else {
                     // 最後尾のヘビ
-                    int lengthOfLastSnake = lengthOfSnakes.getLast();
-                    int indexOfLastSnake = indexOfSnakes.getLast();
-                    indexOfSnakes.add(indexOfLastSnake + lengthOfLastSnake);
+                    long indexOfLastSnake = headIndexOfSnakes.get(headIndexOfSnakes.size() - 1);
+                    long lengthOfLastSnake = lengthOfSnakes.get(lengthOfSnakes.size() - 1);
+                    headIndexOfSnakes.add(indexOfLastSnake + lengthOfLastSnake);
                 }
                 lengthOfSnakes.add(snakeLength);
-            } else if (queryType == 2) {
-                // type 2(remove snake)
-                System.out.println("output: " + queryType);
-                // 先頭のヘビを削除
-                lengthOfSnakes.removeFirst();
-            } else {
-                int snakeLength = sc.nextInt();
-                // type 3(output the index of the snake)
-                int index = indexOfSnakes.getLast();
-                System.out.println(index);
+            }
+            public static void main(String[] args) {
+                List<Long> lengthOfSnakes = new ArrayList<>();
+                List<Long> headIndexOfSnakes = new ArrayList<>();
+                StringBuilder outputSnakes = new StringBuilder();
+
+                long shift = 0;
+                int head = 0;
+
+                Scanner sc = new Scanner(System.in);
+                int numberOfQuery = sc.nextInt();
+
+                for(int i = 0; i < numberOfQuery; i++) {
+                    int queryType = sc.nextInt();
+                    switch (queryType) {
+                        case 1 -> {
+                            long snakeLength = sc.nextLong();
+                            addSnake(snakeLength, lengthOfSnakes, headIndexOfSnakes);
+                        }
+                        case 2 -> {
+                            shift += lengthOfSnakes.get(head);
+                            head++;
+                        }
+                        case 3 -> {
+                            // type 3(output the index of the snake)
+                            int indexOfSnake = sc.nextInt();
+                            // 指定されたhead + indexOfSnake番目を取得
+                            int index = head + indexOfSnake - 1;
+                            // 計算量削減のため最後にまとめて出力する
+                            long result = headIndexOfSnakes.get(index) - shift;
+                            outputSnakes.append(result).append("\n");
+                        }
+                    }
+                }
+                System.out.println(outputSnakes);
             }
         }
-        System.out.println("lengthOfSnakes: " + lengthOfSnakes);
-        System.out.println("indexOfSnake: " + indexOfSnakes);
-    }
-    public static void main(String[] args) {
-        getInputs();
-    }
-}
